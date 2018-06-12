775f94c3b257fbbd650273fe6fabf3a467d77938
nelson88 <nelsonjp@live.com>
2018-06-12 16:46:33 +0530

    Version 0.1.20

--- calendar.module.chunk.js
+++ calendar.module.chunk.js
@@ -3,7 +3,7 @@ webpackJsonp(["calendar.module"],{
 /***/ "./src/app/calendar/calendar.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "\r\n<title>Aspect Calendar</title>\n<div class=\"main-content\">\n    <div class=\"container-fluid\">\n        <div class=\"header text-center\">\n            <h3 class=\"title\">FullCalendar.io</h3>\n            <p class=\"category\">Handcrafted by our friends from\n                <a target=\"_blank\" href=\"https://fullcalendar.io/\">FullCalendar.io</a>. Please checkout their\n                <a href=\"https://fullcalendar.io/docs/\" target=\"_blank\">full documentation.</a>\n            </p>\n        </div>\n        <div class=\"row\">\n            <div class=\"col-md-10 col-md-offset-1\">\n                <div class=\"card card-calendar\">\n                    <div class=\"card-content\" class=\"ps-child\">\n                        <div id=\"fullCalendar\"></div>\n                    </div>\n                </div>\n            </div>\n        </div>\n    </div>\n</div>\n"
+module.exports = "\r\n<title>Aspect Calendar</title>\n<div class=\"main-content\">\n    <div class=\"container-fluid\">\n        <div class=\"toolbar\">\r\n        </div>\n        <form [formGroup]=\"calendarForm\">\r\n            <div class=\"row\">\r\n                <div class=\"col-md-12\">\r\n                    <div class=\"col-sm-3\">\r\n                        <div class=\"card\">\r\n                            <div class=\"card-content\">\r\n                                <div class=\"form-group\">\r\n                                    <label class=\"control-label\">\r\n                                        Select Division\r\n                                    </label>\r\n                                    <div class=\"checkbox form-horizontal-checkbox\" *ngFor=\"let division of divisionList\" style=\"top:-10px;left:10px;\">\r\n                                        <label>\r\n                                            <input type=\"checkbox\" formControlName=\"divChk\" name=\"division\" [id]=\"division.ID\" [value]=\"division.ID\">\r\n                                            {{division.Name}}\r\n                                        </label>\r\n                                    </div>\r\n                                </div>\r\n                            </div>\r\n                        </div>\r\n                    </div>\r\n                </div>\r\n            </div>\r\n            <div class=\"row\">\r\n                <div class=\"col-md-10 col-md-offset-1\">\r\n                    <div class=\"card card-calendar\">\r\n                        <div class=\"card-content ps-child\">\r\n                            <div id=\"fullCalendar\"></div>\r\n                        </div>\r\n                    </div>\r\n                </div>\r\n            </div>\r\n        </form>\r\n    </div>\n</div>\n"
 
 /***/ }),
 
@@ -13,6 +13,12 @@ module.exports = "\r\n<title>Aspect Calendar</title>\n<div class=\"main-content\
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return CalendarComponent; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_forms__ = __webpack_require__("./node_modules/@angular/forms/esm5/forms.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_http__ = __webpack_require__("./node_modules/@angular/http/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_router__ = __webpack_require__("./node_modules/@angular/router/esm5/router.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_rxjs_Rx__ = __webpack_require__("./node_modules/rxjs/_esm5/Rx.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__services_commonService__ = __webpack_require__("./src/app/services/commonService.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__services_divisionService__ = __webpack_require__("./src/app/services/divisionService.ts");
 // IMPORTANT: this is a plugin which requires jQuery for initialisation and data manipulation
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -20,140 +26,184 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+
 
 var CalendarComponent = /** @class */ (function () {
-    function CalendarComponent() {
+    function CalendarComponent(http, router, formBuilder, commonService, divisionService, activatedRoute) {
+        this.http = http;
+        this.router = router;
+        this.formBuilder = formBuilder;
+        this.commonService = commonService;
+        this.divisionService = divisionService;
+        this.activatedRoute = activatedRoute;
+        this.calendarForm = new __WEBPACK_IMPORTED_MODULE_1__angular_forms__["FormGroup"]({
+            divChk: new __WEBPACK_IMPORTED_MODULE_1__angular_forms__["FormControl"]()
+        });
     }
     CalendarComponent.prototype.ngOnInit = function () {
-        var $calendar = $('#fullCalendar');
-        var today = new Date();
-        var y = today.getFullYear();
-        var m = today.getMonth();
-        var d = today.getDate();
-        $calendar.fullCalendar({
-            viewRender: function (view, element) {
-                // We make sure that we activate the perfect scrollbar when the view isn't on Month
-                if (view.name !== 'month') {
-                    var $fc_scroller = $('.fc-scroller');
-                    $fc_scroller.perfectScrollbar();
-                }
-            },
-            header: {
-                left: 'title',
-                center: 'month, agendaWeek, agendaDay',
-                right: 'prev, next, today'
-            },
-            defaultDate: today,
-            selectable: true,
-            selectHelper: true,
-            views: {
-                month: {
-                    titleFormat: 'MMMM YYYY'
-                    // other view-specific options here
-                },
-                week: {
-                    titleFormat: ' MMMM D YYYY'
-                },
-                day: {
-                    titleFormat: 'D MMM, YYYY'
-                }
-            },
-            select: function (start, end) {
-                // on select we show the Sweet Alert modal with an input
-                swal({
-                    title: 'Create an Event',
-                    html: '<div class="form-group">' +
-                        '<input class="form-control" placeholder="Event Title" id="input-field">' +
-                        '</div>',
-                    showCancelButton: true,
-                    confirmButtonClass: 'btn btn-success',
-                    cancelButtonClass: 'btn btn-danger',
-                    buttonsStyling: false
-                }).then(function (result) {
-                    var eventData;
-                    var event_title = $('#input-field').val();
-                    if (event_title) {
-                        eventData = {
-                            title: event_title,
-                            start: start,
-                            end: end
-                        };
-                        $calendar.fullCalendar('renderEvent', eventData, true); // stick? = true
+        if (JSON.parse(sessionStorage.getItem('LogEmployee')) != null) {
+            this.GetAllDivisions();
+            var $calendar_1 = $('#fullCalendar');
+            var today = new Date();
+            var y = today.getFullYear();
+            var m = today.getMonth();
+            var d = today.getDate();
+            $calendar_1.fullCalendar({
+                viewRender: function (view, element) {
+                    // We make sure that we activate the perfect scrollbar when the view isn't on Month
+                    if (view.name !== 'month') {
+                        var $fc_scroller = $('.fc-scroller');
+                        $fc_scroller.perfectScrollbar();
                     }
-                    $calendar.fullCalendar('unselect');
-                });
-            },
-            editable: true,
-            eventLimit: true,
-            // color classes: [ event-blue | event-azure | event-green | event-orange | event-red ]
-            events: [
-                {
-                    title: 'All Day Event',
-                    start: new Date(y, m, 1),
-                    className: 'event-default'
                 },
-                {
-                    id: 999,
-                    title: 'Repeating Event',
-                    start: new Date(y, m, d - 4, 6, 0),
-                    allDay: false,
-                    className: 'event-rose'
+                header: {
+                    left: 'title',
+                    center: 'month, agendaWeek, agendaDay',
+                    right: 'prev, next, today'
                 },
-                {
-                    id: 999,
-                    title: 'Repeating Event',
-                    start: new Date(y, m, d + 3, 6, 0),
-                    allDay: false,
-                    className: 'event-rose'
+                defaultDate: today,
+                selectable: true,
+                selectHelper: true,
+                views: {
+                    month: {
+                        titleFormat: 'MMMM YYYY'
+                        // other view-specific options here
+                    },
+                    week: {
+                        titleFormat: ' MMMM D YYYY'
+                    },
+                    day: {
+                        titleFormat: 'D MMM, YYYY'
+                    }
                 },
-                {
-                    title: 'Meeting',
-                    start: new Date(y, m, d - 1, 10, 30),
-                    allDay: false,
-                    className: 'event-green'
+                select: function (start, end) {
+                    // on select we show the Sweet Alert modal with an input
+                    swal({
+                        title: 'Create an Event',
+                        html: '<div class="form-group">' +
+                            '<input class="form-control" placeholder="Event Title" id="input-field">' +
+                            '</div>',
+                        showCancelButton: true,
+                        confirmButtonClass: 'btn btn-success',
+                        cancelButtonClass: 'btn btn-danger',
+                        buttonsStyling: false
+                    }).then(function (result) {
+                        var eventData;
+                        var event_title = $('#input-field').val();
+                        if (event_title) {
+                            eventData = {
+                                title: event_title,
+                                start: start,
+                                end: end
+                            };
+                            $calendar_1.fullCalendar('renderEvent', eventData, true); // stick? = true
+                        }
+                        $calendar_1.fullCalendar('unselect');
+                    });
                 },
-                {
-                    title: 'Lunch',
-                    start: new Date(y, m, d + 7, 12, 0),
-                    end: new Date(y, m, d + 7, 14, 0),
-                    allDay: false,
-                    className: 'event-red'
-                },
-                {
-                    title: 'Md-pro Launch',
-                    start: new Date(y, m, d - 2, 12, 0),
-                    allDay: true,
-                    className: 'event-azure'
-                },
-                {
-                    title: 'Birthday Party',
-                    start: new Date(y, m, d + 1, 19, 0),
-                    end: new Date(y, m, d + 1, 22, 30),
-                    allDay: false,
-                    className: 'event-azure'
-                },
-                {
-                    title: 'Click for Creative Tim',
-                    start: new Date(y, m, 21),
-                    end: new Date(y, m, 22),
-                    url: 'https://www.creative-tim.com/',
-                    className: 'event-orange'
-                },
-                {
-                    title: 'Click for Google',
-                    start: new Date(y, m, 21),
-                    end: new Date(y, m, 22),
-                    url: 'https://www.creative-tim.com/',
-                    className: 'event-orange'
-                }
-            ]
+                editable: true,
+                eventLimit: true,
+                // color classes: [ event-blue | event-azure | event-green | event-orange | event-red ]
+                events: [
+                    {
+                        title: 'All Day Event',
+                        start: new Date(y, m, 1),
+                        className: 'event-default'
+                    },
+                    {
+                        id: 999,
+                        title: 'Repeating Event',
+                        start: new Date(y, m, d - 4, 6, 0),
+                        allDay: false,
+                        className: 'event-rose'
+                    },
+                    {
+                        id: 999,
+                        title: 'Repeating Event',
+                        start: new Date(y, m, d + 3, 6, 0),
+                        allDay: false,
+                        className: 'event-rose'
+                    },
+                    {
+                        title: 'Meeting',
+                        start: new Date(y, m, d - 1, 10, 30),
+                        allDay: false,
+                        className: 'event-green'
+                    },
+                    {
+                        title: 'Lunch',
+                        start: new Date(y, m, d + 7, 12, 0),
+                        end: new Date(y, m, d + 7, 14, 0),
+                        allDay: false,
+                        className: 'event-red'
+                    },
+                    {
+                        title: 'Md-pro Launch',
+                        start: new Date(y, m, d - 2, 12, 0),
+                        allDay: true,
+                        className: 'event-azure'
+                    },
+                    {
+                        title: 'Birthday Party',
+                        start: new Date(y, m, d + 1, 19, 0),
+                        end: new Date(y, m, d + 1, 22, 30),
+                        allDay: false,
+                        className: 'event-azure'
+                    },
+                    {
+                        title: 'Click for Creative Tim',
+                        start: new Date(y, m, 21),
+                        end: new Date(y, m, 22),
+                        url: 'https://www.creative-tim.com/',
+                        className: 'event-orange'
+                    },
+                    {
+                        title: 'Click for Google',
+                        start: new Date(y, m, 21),
+                        end: new Date(y, m, 22),
+                        url: 'https://www.creative-tim.com/',
+                        className: 'event-orange'
+                    }
+                ]
+            });
+        }
+        else {
+            this.router.navigate(['pages/login']);
+        }
+    };
+    CalendarComponent.prototype.GetAllDivisions = function () {
+        var _this = this;
+        $('.loader').show();
+        this.divisionService.GetAllDivisions(this.commonService.baseApiUrl).subscribe(function (data) {
+            var dataDiv = JSON.stringify(data);
+            var divParse = JSON.parse(dataDiv);
+            var divList = JSON.parse(divParse["_body"]);
+            if (divList != null) {
+                _this.divisionList = divList;
+                console.log(_this.divisionList);
+            }
         });
     };
     CalendarComponent = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
             selector: 'app-calendar-cmp',
-            template: __webpack_require__("./src/app/calendar/calendar.component.html")
-        })
+            template: __webpack_require__("./src/app/calendar/calendar.component.html"),
+            providers: [__WEBPACK_IMPORTED_MODULE_5__services_commonService__["a" /* CommonService */], __WEBPACK_IMPORTED_MODULE_6__services_divisionService__["a" /* DivisionService */]]
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2__angular_http__["Http"],
+            __WEBPACK_IMPORTED_MODULE_3__angular_router__["d" /* Router */],
+            __WEBPACK_IMPORTED_MODULE_1__angular_forms__["FormBuilder"],
+            __WEBPACK_IMPORTED_MODULE_5__services_commonService__["a" /* CommonService */],
+            __WEBPACK_IMPORTED_MODULE_6__services_divisionService__["a" /* DivisionService */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_router__["a" /* ActivatedRoute */]])
     ], CalendarComponent);
     return CalendarComponent;
 }());
@@ -195,7 +245,8 @@ var CalendarModule = /** @class */ (function () {
             imports: [
                 __WEBPACK_IMPORTED_MODULE_2__angular_common__["CommonModule"],
                 __WEBPACK_IMPORTED_MODULE_1__angular_router__["e" /* RouterModule */].forChild(__WEBPACK_IMPORTED_MODULE_5__calendar_routing__["a" /* CalendarRoutes */]),
-                __WEBPACK_IMPORTED_MODULE_3__angular_forms__["FormsModule"]
+                __WEBPACK_IMPORTED_MODULE_3__angular_forms__["FormsModule"],
+                __WEBPACK_IMPORTED_MODULE_3__angular_forms__["ReactiveFormsModule"]
             ],
             declarations: [__WEBPACK_IMPORTED_MODULE_4__calendar_component__["a" /* CalendarComponent */]]
         })
--- common.chunk.js
+++ common.chunk.js
@@ -41308,6 +41308,47 @@ var CountryService = /** @class */ (function () {
 
 /***/ }),
 
+/***/ "./src/app/services/divisionService.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return DivisionService; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_http__ = __webpack_require__("./node_modules/@angular/http/esm5/http.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var DivisionService = /** @class */ (function () {
+    function DivisionService(http) {
+        this.http = http;
+    }
+    DivisionService.prototype.GetAllDivisions = function (baseurl) {
+        return this.http.get(baseurl + "GetAllDivision/");
+    };
+    ;
+    DivisionService.prototype.GetDivisionByUser = function (baseurl, contact) {
+        return this.http.get(baseurl + "GetDivisionByUser/" + contact);
+    };
+    ;
+    DivisionService = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_http__["Http"]])
+    ], DivisionService);
+    return DivisionService;
+}());
+
+
+
+/***/ }),
+
 /***/ "./src/app/services/maintenanceService.ts":
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
